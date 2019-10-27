b047753f9a01556e2c59ec03b6513eb97e39af8d
Christian Chiang <crchiang@middlebury.edu>
2019-10-27 19:47:55 -0400

    Basic template of homepage. App-Search component contains all the parameters for filtering a search. No functional Typescript behind it yet.

---
+++ angular-frontend/src/app/dummy-data.ts
@@ -0,0 +1,7 @@
+import { Room } from "./room"
+
+export const ROOMS: Room[] = [
+    {buildingName: "ATA", roomID: "B212E", occupancy: 4, sqft: 1136, description: "example Atwater suite."},
+    {buildingName: "BTC", roomID: "305", occupancy: 2, sqft: 184, description: "example Battell double."},
+    {buildingName: "LGW", roomID: "101", occupancy: 1, sqft: 144, description: "example Language House single."}
+]
---
+++ angular-frontend/src/app/room.ts
@@ -0,0 +1,10 @@
+export class Room {
+    buildingName: string;
+    roomID: string;
+    occupancy: number;
+    sqft: number;
+    /* bType: string;
+    commons: string;
+    year: string; */
+    description: string;
+}
