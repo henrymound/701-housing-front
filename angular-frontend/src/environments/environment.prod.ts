c9656da2c70e3bfbe62750ad54c26082fd34f58f
Henry Mound <henrymound@gmail.com>
2019-12-04 17:53:45 -0500

    Finished deploy of new version to firebase hosting (fixed environment.prod.ts)

--- angular-frontend/src/environments/environment.prod.ts
+++ angular-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDCOAuNwzd0PYBKmd2VPGSG1c6ojZuciQ8",
+    authDomain: "midd-housing-database.firebaseapp.com",
+    databaseURL: "https://midd-housing-database.firebaseio.com",
+    projectId: "midd-housing-database",
+    storageBucket: "midd-housing-database.appspot.com",
+    messagingSenderId: "988504115884",
+    appId: "1:988504115884:web:78f6a5b31634c60673ed09",
+    measurementId: "G-2CQ3LTHVGC"
+  }
 };
